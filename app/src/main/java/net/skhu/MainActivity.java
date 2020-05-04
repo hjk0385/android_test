ded6e22f475e52039e24bf31fd4c150e7217f723
Kim, Hyunjung <hjk0385@gmail.com>
2020-05-04 17:14:35 +0900

    2차

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -1,5 +1,6 @@
 package net.skhu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,14 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button exam1 = (Button) findViewById(R.id.exam1);
+
+        Button button = (Button)findViewById(R.id.exam1);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new 
+            }
+
+        });
     }
 
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(getApplicationContext(), SubActivity.class);
-            startActivity(intent);
-        }
-    }
 }
