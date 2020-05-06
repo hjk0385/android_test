904eb26d74a489cdf4982de820782fbb83a9ee14
Kim, Hyunjung <hjk0385@gmail.com>
2020-05-06 17:12:14 +0900

    2차

--- app/src/main/java/net/skhu/Exam3Activity.java
+++ app/src/main/java/net/skhu/Exam3Activity.java
@@ -1,14 +1,28 @@
 package net.skhu;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle; 
+import java.util.ArrayList;
 
 public class Exam3Activity extends AppCompatActivity {
+    RecyclerView1Adapter recyclerView1Adapter;
+    ArrayList<String> arrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam3);
+        arrayList = new ArrayList<String>();
+        arrayList.add("one");
+        arrayList.add("two");
+        recyclerView1Adapter = new RecyclerView1Adapter(this, arrayList);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerView1Adapter);
     }
 }
--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -2,7 +2,11 @@ package net.skhu;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button3 = (Button) findViewById(R.id.button3) ;
+        button3.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view) {
+        if(view.getId()==R.id.button3){
+            Intent intent=new Intent(this, Exam3Activity.class);
+            startActivity(intent);
+        }
     }
 }
